1f47f6944e4648e7c8d330a857e440de7ab5a2c9
Shubham Singh <shubham88singh@gmail.com>
2020-08-25 03:33:36 +0530

    Adding the tailwind library to the app

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import React from 'react';
 import './App.css';
 
 function App() {
-  return <div className="App">Hiiii</div>;
+  return <div className="App bg-red-400">Hiiii</div>;
 }
 
 export default App;
