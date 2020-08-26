3959a80b4493aeeb420b7a0efcddaa1c8b15ac51
Shubham Singh <shubham88singh@gmail.com>
2020-08-26 21:06:35 +0530

    Adding the routes for the houses card

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import './App.css';
 import SignUp from './pages/Signup';
 import Login from './pages/Login';
 import Home from './pages/Home';
+import Description from './pages/Description';
 function App() {
   return (
     <div className="max-w-lg mx-auto my-12">
@@ -17,6 +18,9 @@ function App() {
           </Route>
           <Route exact path="/login">
             <Login />
+          </Route>
+          <Route exact path="/houses/:id">
+            <Description />
           </Route>
         </Switch>
       </Router>
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -4,6 +4,7 @@ import { useDispatch, useSelector } from 'react-redux';
 import { getHouses } from '../actions';
 import { getHousesData } from '../selectors';
 import Card from '../components/Card';
+import { Link } from 'react-router-dom';
 const Home = () => {
   const dispatch = useDispatch();
   const houses = useSelector(getHousesData);
@@ -13,7 +14,7 @@ const Home = () => {
   }, [dispatch]);
 
   return (
-    <div className>
+    <div className="hiio">
       <div className="flex justify-around">
         <FiMenu />
         <h2 className="text-lg font-bold">Houses</h2>
@@ -21,7 +22,12 @@ const Home = () => {
       </div>
       <div className="my-8 p-2 flex flex-col items-center">
         {houses.length !== 0
-          ? houses.map((house) => <Card house={house} />)
+          ? houses.map((house) => (
+              <Link key={house.id} to={`/houses/${house.id}`}>
+                {' '}
+                <Card house={house} />
+              </Link>
+            ))
           : null}
       </div>
     </div>
