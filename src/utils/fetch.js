98b1394eba045cebafb8d1e9adba42108828b775
Shubham Singh <shubham88singh@gmail.com>
2020-08-28 05:02:43 +0530

    Fixing the linter and link

--- src/utils/fetch.js
+++ src/utils/fetch.js
@@ -1,4 +1,4 @@
 import axios from 'axios';
 
 // eslint-disable-next-line import/prefer-default-export
-export const publicFetch = axios.create({ baseURL: "https://glacial-reef-12059.herokuapp.com/" });
+export const publicFetch = axios.create({ baseURL: 'https://glacial-reef-12059.herokuapp.com/' });
