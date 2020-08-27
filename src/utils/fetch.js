5b72432c683ed754174ce32661ce2246bb78cf7d
Shubham Singh <shubham88singh@gmail.com>
2020-08-28 04:30:22 +0530

    Adding the url for the backend

--- src/utils/fetch.js
+++ src/utils/fetch.js
@@ -1,4 +1,4 @@
 import axios from 'axios';
 
 // eslint-disable-next-line import/prefer-default-export
-export const publicFetch = axios.create({ baseURL: process.env.REACT_APP_URL });
+export const publicFetch = axios.create({ baseURL: "https://glacial-reef-12059.herokuapp.com/" });
