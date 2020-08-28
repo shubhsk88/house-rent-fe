4d9bb89ef1c908196f57357c69f13e8d16ee2850
Shubham Singh <shubham88singh@gmail.com>
2020-08-28 06:13:58 +0530

    Fixing the style lint error

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import './App.css';
+
 import SignUp from './pages/Signup';
 import Login from './pages/Login';
 import Home from './pages/Home';
